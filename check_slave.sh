1486a5cf82f876ba91742e6225561049a2dcde3f
markgruen <veryverygoofy@gmail.com>
2016-04-08 07:21:27 -0400

    added force option
    Added force option to check_slave.sh to force sending email no matter
    what the status

--- check_slave.sh
+++ check_slave.sh
@@ -3,37 +3,58 @@
 emails="mgruenberg@test.com,test1@test.com,test2@test.com"
 
 send_email=0
+force=0
 
-USAGE="USAGE: $(basename $0) [-m] [-h|-?]"
+USAGE="USAGE: $(basename $0) [-mf] [-h|-?]"
 
-while getopts :mh opt
+function printout
+{
+    echo "Date: ${date}"
+    echo "Status: $status"
+    echo "error level: $error_level"
+    echo "$out"
+}
+
+function do_sendmail
+{
+    echo "$out" | mutt -s "[SLAVE $error_level] [$host]  ${date}" "$emails"
+}
+
+while getopts :mfh opt
 do
     case $opt in
         m) send_email=1;;
+        f) force=1;;
         ?|h) echo $USAGE
             exit;;
     esac
 done
 
-out=$(/home/mgruen/scripts/mysql_test_slave_status1.py --verbose-pretty)
+date="$(date "+%a %F %T")"
+host="$(hostname | tr '[:lower:]' '[:upper:]')"
+out=$(/home/mgruen/scripts/mysql_test_slave_status.py --verbose-pretty)
 status=$?
 
-if [ $status -ne 0 ]; then
-    if [ $status -eq 3 ]; then 
-        error_level="WARNING"
-    else
-        error_level="ERROR"
-    fi
-    host="$(hostname | tr '[:lower:]' '[:upper:]')"
-    #echo "[SLAVE $error_level] [$host]  $(date "+%Y-%m-%d %T")"
-    echo "Date: $(date "+%Y-%m-%d %T")"
-    echo "Status: $status"
-    echo "error level: $error_level"
-    echo "$out"
-    if [ $send_email -eq 1 ]; then 
-        echo "$out" | mutt -s "[SLAVE $error_level] [$host]  $(date "+%Y-%m-%d %T")" "$emails"
-    fi
-elif [ $send_email -eq 0 ]; then
-    echo $(date "+%Y-%m-%d %T")
-    echo $out
+#    Exit Codes:
+#        0 all is okay
+#        1 sql thread is dead
+#        2 slave isnt running
+#        3 behind
+#        -1 error
+
+error_level=""
+case $status in
+    1|2|-1) error_level="ERROR";;
+    3) error_level="WARNING";;
+    0) error_level="NORMAL"
+esac
+
+if [ $status -ne 0 -a $send_email -eq 1 ]; then
+    printout
+    do_sendmail
+elif [ $force -eq 1 ]; then
+    printout
+    do_sendmail
+else
+    printout
 fi
